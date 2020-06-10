352117a86a21bf8046405b21c57bf12f125ab195
claudia v <claudia4ro@yahoo.com>
2020-06-10 01:07:44 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     list_characters =self.characters
-    list_characters.map{ |i| "#{i.name} - #{i.show_id}"}
+    list_characters.map{ |i| "#{i.name} - #{i.show}"}
     
   end 
   
